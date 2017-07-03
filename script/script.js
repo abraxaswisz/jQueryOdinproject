bb7400008eb5c95d6e29f134e03de86511b73f96
Bartosz Dryl <abraxaswisz@gmail.com>
2017-07-03 14:23:53 +0200

    it's working now!

--- script/script.js
+++ script/script.js
@@ -1,3 +1,18 @@
-$(document).ready(function(){
-	alert('hello world');
+$(document).ready(function () {
+	function siatkaKomorek() {
+	let komorki = prompt('ile komorek?');
+    let $dodajPixel = $('<div class="onepixel"></div>').text("");
+    for (let i = 0; i <= komorki; i++) {
+        $dodajPixel = $('<div class="onepixel"></div>').text("");
+        $('.pudlo').append($dodajPixel);
+    }
+}
+siatkaKomorek();
+
+$('.onepixel').on('mousehold', function(){
+	$(this).addClass('zmianakoloru');
+
+})
+
+
 });
