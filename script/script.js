da80bf637d3d8e2dab7962417f9def4ad340c02f
Bartosz Dryl <abraxaswisz@gmail.com>
2017-07-03 15:26:10 +0200

    changed index.html, fixed some stuff

--- script/script.js
+++ script/script.js
@@ -1,18 +1,24 @@
 $(document).ready(function () {
 	function siatkaKomorek() {
-	let komorki = prompt('ile komorek?');
-    let $dodajPixel = $('<div class="onepixel"></div>').text("");
-    for (let i = 0; i <= komorki; i++) {
-        $dodajPixel = $('<div class="onepixel"></div>').text("");
-        $('.pudlo').append($dodajPixel);
-    }
-}
-siatkaKomorek();
 
-$('.onepixel').on('mousehold', function(){
-	$(this).addClass('zmianakoloru');
+		let komorki = prompt('ile komorek');
+		let $dodajPixel = $('<div class="onepixel"></div>').text("");
+		for (let i = 0; i <= komorki; i++) {
+			$dodajPixel = $('<div class="onepixel"></div>').text("");
+			$('.pudlo').append($dodajPixel);
+		}
+	}
+	siatkaKomorek();
 
-})
+	$('.onepixel').on('click', function(){
+		$(this).addClass('zmianakoloru');
+		if($('.onepixel').hasClass('zmianakoloru')) {
+			$('.onepixel').on('mouseenter', function(){
+				$(this).addClass('zmianakoloru');
+			});
+		}
+
+	})
 
 
 });
